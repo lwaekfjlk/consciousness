a94e8847f3f58d3e928fd7251a651a920c40243b
lwaekfjlk <1125027232@qq.com>
2023-07-13 14:25:06 -0400

    🚀 start designing humor instruction tuning

--- humor/caption_contest_corpus/gpt4/run.sh
+++ humor/caption_contest_corpus/gpt4/run.sh
@@ -1,8 +1,8 @@
 for split in 0;
   do for shots in 5;
     do for task in matching;
-      do for engine in 'gpt-3.5-turbo';
-        do python gpt4_from_description.py --engine $engine --task $task --val 0 --split $split --shots $shots;
+      do for engine in 'gpt-4';
+        do python gpt4_from_description_answer.py --engine $engine --task $task --val 0 --split $split --shots $shots;
       done;
     done;
   done;
